c91afdc9b1ef4c7b81b8aea3166a4956957960cb
Unknown <60010465@kmitl.ac.th>
2018-03-22 16:27:40 +0700

    update fetch data to firebase

--- miniProject.ino
+++ miniProject.ino
@@ -3,13 +3,15 @@
 #include <FirebaseArduino.h>
 #include <ArduinoJson.h>
 
-#define WIFI_SSID       "Planx"
+#define WIFI_SSID       "Planxnx"
 #define WIFI_PASSWORD   "planxthanee"
 
 #define FIREBASE_HOST "miniproject-2560.firebaseio.com"
 #define FIREBASE_KEY "KRo5ya1izH6Ae7CXbqAUT62UMRScvnkrlqZ5ZPYg"
 
 DHT dht;//สร้างออบเจกค
+int rounds=0;
+float arTemp[61],avgTemp,tTemp;
 
 void setup()
 {
@@ -28,32 +30,52 @@ void setup()
   Firebase.begin(FIREBASE_HOST, FIREBASE_KEY);
 
   Serial.println();
-  Serial.println("Status \tHumidity (%)\tTemperature (C)\t(F)");
-
+  
+  //Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+  
   dht.setup(4); // data pin 2
 }
 
 void loop()
 {
+  rounds++;
 
   unsigned long Time = millis();//หาร1พันได้1วิ
   
-  delay(dht.getMinimumSamplingPeriod());
-
+  float x = Time;// เปลี่ยนค่าเวลาให้เป็นทศนิยม
+  float times = x/1000;//แปลงหน่วย
   float humidity = dht.getHumidity(); // ดึงค่าความชื้น
   float temperature = dht.getTemperature(); // ดึงค่าอุณหภูมิ
   
-  Firebase.setInt("Times", Time);
+  Firebase.setInt("Times", times);
   Firebase.setFloat("Humidity", humidity);
   Firebase.setFloat("Temp", temperature);
   
-  Serial.print(Time);
-  Serial.print(" s\t");
+  Serial.print(rounds);
+  Serial.print("  ");
+  Serial.print(times);
+  Serial.print("s \t");
   Serial.print(dht.getStatusString());
   Serial.print("\t");
   Serial.print(humidity, 1);
   Serial.print("\t\t");
   Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.println(dht.toFahrenheit(temperature), 1);
+  Serial.print("C\t\t");
+  Serial.print(dht.toFahrenheit(temperature), 1);
+  Serial.println("F\t\t");
+
+  arTemp[rounds]=dht.getTemperature();
+  if(rounds%60==0){
+    for(int i=1;i<=60;i++){
+      tTemp+=arTemp[i];
+    }
+    avgTemp = tTemp/60;
+    Firebase.setFloat("AvgTemp", avgTemp);
+    Serial.print(avgTemp);
+    Serial.println("C (avg)");
+    rounds=0;
+    tTemp=0;
+  }
+  
+  //delay(dht.getMinimumSamplingPeriod());
 }
