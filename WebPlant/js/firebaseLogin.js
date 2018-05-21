0f3170d9ef3ca9af4323d45fc89c814341d33de3
Unknown <60010465@kmitl.ac.th>
2018-05-22 01:35:18 +0700

    fix

--- WebPlant/js/firebaseLogin.js
+++ WebPlant/js/firebaseLogin.js
@@ -1,12 +1,13 @@
-var config = {
-    apiKey: "AIzaSyCfUxVqAaobZHxwCeOFHJiAOKTFX4T-H6o",  
-    authDomain: "miniproject-2560.firebaseapp.com",
-    databaseURL: "https://miniproject-2560.firebaseio.com",
-    projectId: "miniproject-2560",
-    storageBucket: "miniproject-2560.appspot.com",
-    messagingSenderId: "833789294851"
-  };
-  firebase.initializeApp(config);
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyCfUxVqAaobZHxwCeOFHJiAOKTFX4T-H6o",
+  authDomain: "miniproject-2560.firebaseapp.com",
+  databaseURL: "https://miniproject-2560.firebaseio.com",
+  projectId: "miniproject-2560",
+  storageBucket: "miniproject-2560.appspot.com",
+  messagingSenderId: "833789294851"
+};
+firebase.initializeApp(config);
   
   //auth การรับรองสิทธ
   /*firebase.auth().signInWithEmailAndPassword("admin@test.com", "123456789").catch(function(error) {
