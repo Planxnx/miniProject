0fbb586bebf414a66793b84e73cc9ce7ca273c5b
Unknown <60010465@kmitl.ac.th>
2018-05-22 02:19:38 +0700

    update mobile site

---
+++ WebPlant/mobilepage/js/startup.js
@@ -0,0 +1,6 @@
+var i = 0 ;
+for( ;i<=1;i++){
+    firebase.database().ref('Plants' ).set("รอสักครู่...");
+    firebase.database().ref('Temp' ).set("รอสักครู่...");
+    firebase.database().ref('timeCounts' ).set("Arduino ยังไม่ทำงาน");      
+}
