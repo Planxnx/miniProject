51558fe5e820e229317fcfc378a931d4e3ef37af
Unknown <60010465@kmitl.ac.th>
2018-05-24 19:26:14 +0700

    remove junk

--- WebPlant Moblie/js/startup.js
+++
@@ -1,6 +0,0 @@
-var i = 0 ;
-for( ;i<=1;i++){
-    firebase.database().ref('Plants' ).set("รอสักครู่...");
-    firebase.database().ref('Temp' ).set("รอสักครู่...");
-    firebase.database().ref('timeCounts' ).set("Arduino ยังไม่ทำงาน");      
-}
